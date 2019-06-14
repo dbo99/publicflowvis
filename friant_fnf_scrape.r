d2508e2db4db91c32cd60463139d06d628daafdd
dbo99 <oconnor.david.b@gmail.com>
2019-06-14 09:49:03 -0700

    Add files via upload

--- friant_fnf_scrape.r
+++ friant_fnf_scrape.r
@@ -3,6 +3,7 @@
 {
 library(pdftools)
 file <- "https://www.usbr.gov/mp/cvo/vungvari/milfln.pdf"
+#file <- "https://www.usbr.gov/mp/cvo/vungvari/trndop.pdf"
 rawtext  <- pdf_text(file)
 
 start <- "\r\n    1"
@@ -11,23 +12,35 @@ friant_fnf <- read.table(text=substring(rawtext, regexpr(start, rawtext), regexp
 as_tibble(friant_fnf)
 nrow_friant_fnf <- nrow(friant_fnf)
 
-friant_fnf <- df[nrow_friant_fnf,]
+friant_fnf <- friant_fnf [nrow_friant_fnf,]
 as_tibble(friant_fnf)
-friant_fnf  <- friant_fnf %>%         rename("monthday" = !!names(.[1]),
-                                              "edison_lake_taf_usbr" = !!names(.[2]),
-                                             "florence_lake_taf_usbr" = !!names(.[3]),
-                                             "huntington_lake_taf_usbr" = !!names(.[4]),
-                                             "shaver_lake_taf_usbr" = !!names(.[5]),
-                                             "mammoth_pool_taf_usbr" = !!names(.[6]) ,
-                                             "redinger_lake_taf_usbr" = !!names(.[7]),
-                                             "crane_valley_taf_usbr" = !!names(.[8]),
-                                             "kerckhoff_lake_taf_usbr" = !!names(.[9]),
-                                             "friant_taf_usbr" = !!names(.[10])  ,                                          
-                                             "friant_change_taf_usbr" = !!names(.[11]),
-                                             "friant_change_cfs_usbr" = !!names(.[12]),
+friant_fnf  <- friant_fnf %>% rename("monthday" = !!names(.[1]),
+                                              "edison_stor_usbr" = !!names(.[2]),
+                                             "florence_stor_usbr" = !!names(.[3]),
+                                             "huntington_stor_usbr" = !!names(.[4]),
+                                             "shaver_stor_usbr" = !!names(.[5]),
+                                             "mammothpool_stor_usbr" = !!names(.[6]) ,
+                                             "redinger_stor_usbr" = !!names(.[7]),
+                                             "cranevalley_stor_usbr" = !!names(.[8]),
+                                             "kerckhoff_stor_usbr" = !!names(.[9]),
+                                             "friant_stor_usbr" = !!names(.[10])  ,                                          
+                                             "friant_dlystorchange_usbr" = !!names(.[11]),
+                                             "friant_dlycfschange_usbr" = !!names(.[12]),
                                              "friant_observedinflow_cfs_usbr" = !!names(.[13]),
-                                             
-                                             "friant_natural_river_fnf" = !!names(.[14])) %>% select(-monthday, -V15)
+                                             "friant_natriver_fnf" = !!names(.[14])) %>% select(-monthday, -V15)
 as_tibble(friant_fnf)
 
+
 }
+
+friant_fnf_t <- friant_fnf %>% gather(key = "res", value = "value")
+
+
+## capacities (from various sources online, including wiki) ##
+
+res_id_nws <- c("TAEC1", "FLEC1", "HNTC1", "SAVC1", "MPLC1", "RGRC1", "BASC1", "KRHC1")
+res_cap <- c(125.0, 64.6, 88.834, 135.283, 123.0, 35.0, 45.4, 4.252)
+res_cap <- data.frame(res_id_nws, res_cap)
+
+
+
