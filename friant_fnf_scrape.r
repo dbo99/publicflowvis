b91519e3c399d8656bf975a97ddf2a2ca7948cc2
dbo99 <oconnor.david.b@gmail.com>
2019-06-17 12:17:33 -0700

    Add files via upload

--- friant_fnf_scrape.r
+++ friant_fnf_scrape.r
@@ -1,4 +1,6 @@
+rm(list = ls()) 
 
+#{
 
 {
 library(pdftools)
@@ -16,107 +18,129 @@ friant_fnf_mostrecent <- friant_fnf[nrow_friant_fnf,]  #most recent row from usb
 friant_fnf_nextmostrecent <- friant_fnf[nrow_friant_fnf - 1,]  #next most recent row from usbr
 as_tibble(friant_fnf_mostrecent)
 as_tibble(friant_fnf_nextmostrecent)
+rm(friant_fnf)
 
 ## rename most recent ## 
 friant_fnf_mostrecent  <- friant_fnf_mostrecent %>% rename("monthday" = !!names(.[1]),
-                                              "edison_stor_usbr" = !!names(.[2]),
-                                             "florence_stor_usbr" = !!names(.[3]),
-                                             "huntington_stor_usbr" = !!names(.[4]),
-                                             "shaver_stor_usbr" = !!names(.[5]),
-                                             "mammothpool_stor_usbr" = !!names(.[6]) ,
-                                             "redinger_stor_usbr" = !!names(.[7]),
-                                             "cranevalley_stor_usbr" = !!names(.[8]),
-                                             "kerckhoff_stor_usbr" = !!names(.[9]),
-                                             "friant_stor_usbr" = !!names(.[10])  ,                                          
-                                             "friant_dlystorchange_usbr" = !!names(.[11]),
-                                             "friant_dlyinflowchange_usbr" = !!names(.[12]),
-                                             "friant_observedinflow_cfs_usbr" = !!names(.[13]),
-                                             "friant_natriver_fnf" = !!names(.[14])) %>% select(-monthday, -V15)
+                                             "Edison" = !!names(.[2]),
+                                             "Florence" = !!names(.[3]),
+                                             "Huntington" = !!names(.[4]),
+                                             "Shaver" = !!names(.[5]),
+                                             "MammothPool" = !!names(.[6]) ,
+                                             "Redinger" = !!names(.[7]),
+                                             "CraneValley" = !!names(.[8]),
+                                             "Kerckhoff" = !!names(.[9]),
+                                             "Millerton_stor_usbr" = !!names(.[10])  ,                                          
+                                             "Millerton_dlystorchange_usbr" = !!names(.[11]),
+                                             "Millerton_dlyinflowchange_usbr" = !!names(.[12]),
+                                             "Millerton_observedinflow_cfs" = !!names(.[13]),
+                                             "Millerton_natriver_fnf" = !!names(.[14])) %>% select(-monthday, -V15)
 as_tibble(friant_fnf_mostrecent)
 
 ## rename next most recent ## 
 
 friant_fnf_nextmostrecent  <- friant_fnf_nextmostrecent %>% rename("monthday" = !!names(.[1]),
-                                                           "edison_stor_usbr" = !!names(.[2]),
-                                                           "florence_stor_usbr" = !!names(.[3]),
-                                                           "huntington_stor_usbr" = !!names(.[4]),
-                                                           "shaver_stor_usbr" = !!names(.[5]),
-                                                           "mammothpool_stor_usbr" = !!names(.[6]) ,
-                                                           "redinger_stor_usbr" = !!names(.[7]),
-                                                           "cranevalley_stor_usbr" = !!names(.[8]),
-                                                           "kerckhoff_stor_usbr" = !!names(.[9]),
-                                                           "friant_stor_usbr" = !!names(.[10])  ,                                          
-                                                           "friant_dlystorchange_usbr" = !!names(.[11]),
-                                                           "friant_dlyinflowchange_usbr" = !!names(.[12]),
-                                                           "friant_observedinflow_cfs_usbr" = !!names(.[13]),
-                                                           "friant_natriver_fnf" = !!names(.[14])) %>% select(-monthday, -V15)
-as_tibble(friant_fnf_nextmostrecent)
-
+                                                                   "Edison" = !!names(.[2]),
+                                                                   "Florence" = !!names(.[3]),
+                                                                   "Huntington" = !!names(.[4]),
+                                                                   "Shaver" = !!names(.[5]),
+                                                                   "MammothPool" = !!names(.[6]) ,
+                                                                   "Redinger" = !!names(.[7]),
+                                                                   "CraneValley" = !!names(.[8]),
+                                                                   "Kerckhoff" = !!names(.[9]),
+                                                                   "Millerton_stor_usbr" = !!names(.[10])  ,                                          
+                                                                   "Millerton_dlystorchange_usbr" = !!names(.[11]),
+                                                                   "Millerton_dlyinflowchange_usbr" = !!names(.[12]),
+                                                                   "Millerton_observedinflow_cfs" = !!names(.[13]),
+                                                                   "Millerton_natriver_fnf" = !!names(.[14])) %>% select(-monthday, -V15)
+as_tibble(friant_fnf_mostrecent)
+}
 ###### convert mostrecent's to long format, add attributes######
-
+{
 friant_fnf_mostrecent_t <- friant_fnf_mostrecent %>% gather(key = "res", value = "value")
 nws_id <- c("TAEC1", "FLEC1", "HNTC1", "SAVC1", "MPLC1", "RGRC1", "BASC1", "KRHC1", "FRAC1", 
                     "FRAC1", "FRAC1", "FRAC1", "FRAC1")
-param <- c("stor_current", "stor_current", "stor_current", "stor_current", "stor_current", "stor_current",
-           "stor_current", "stor_current", "stor_current", "stor_dlychnge", "meancfs_dlychnge", "meancfs_dlyinflowobs",
-           "meancfs_fnf")
+usbr_web_param <- c("stor_latest_cvopublished", "stor_latest_cvopublished", "stor_latest_cvopublished", "stor_latest_cvopublished", "stor_latest_cvopublished", "stor_latest_cvopublished",
+           "stor_latest_cvopublished", "stor_latest_cvopublished", "stor_latest_cvopublished", "stor_dlychnge", "meancfs_dlychnge", "meancfs_dlyinflowobs_latest_cvopublished",
+           "meancfs_fnf_latest_cvopublished")
 
 unit <- c("taf", "taf", "taf", "taf", "taf", "taf",
           "taf", "taf", "taf", "taf", "cfs", "cfs",
           "cfs")
 
-friant_fnf_mostrecent <- cbind(friant_fnf_mostrecent_t, nws_id, param, unit)
+friant_fnf_mostrecent <- cbind(friant_fnf_mostrecent_t, nws_id, usbr_web_param, unit)
+rm(friant_fnf_mostrecent_t, unit, usbr_web_param, nws_id)
 as_tibble(friant_fnf_mostrecent)
 
 friant_fnf_mostrecent$value <- gsub(",", "", friant_fnf_mostrecent$value )
 friant_fnf_mostrecent$value <- gsub("+", "", friant_fnf_mostrecent$value ) #doesn't work, but below's as.numeric() seems to
 as_tibble(friant_fnf_mostrecent)
-friant_fnf_mostrecent <- friant_fnf_mostrecent %>% mutate(value = as.numeric(value))
+friant_fnf_mostrecent <- friant_fnf_mostrecent %>% mutate(value = as.numeric(value)) 
 as_tibble(friant_fnf_mostrecent)
+}
 
+{
+friant_fnf_mostrecent <- friant_fnf_mostrecent %>% 
+                        filter(usbr_web_param != "stor_dlychnge") %>% filter(usbr_web_param != "meancfs_dlychnge")
+as_tibble(friant_fnf_mostrecent)
+}
 ###### convert mostrecent's to long format, add attributes######
 
+{
 friant_fnf_nextmostrecent_t <- friant_fnf_nextmostrecent %>% gather(key = "res", value = "value")
 nws_id <- c("TAEC1", "FLEC1", "HNTC1", "SAVC1", "MPLC1", "RGRC1", "BASC1", "KRHC1", "FRAC1", 
             "FRAC1", "FRAC1", "FRAC1", "FRAC1")
-param <- c("stor_current", "stor_current", "stor_current", "stor_current", "stor_current", "stor_current",
-           "stor_current", "stor_current", "stor_current", "stor_dlychnge", "meancfs_dlychnge", "meancfs_dlyinflowobs",
-           "meancfs_fnf")
+usbr_web_param <- c("stor_nextlatest_cvopublished", "stor_nextlatest_cvopublished", "stor_nextlatest_cvopublished", "stor_nextlatest_cvopublished", "stor_nextlatest_cvopublished", "stor_nextlatest_cvopublished",
+           "stor_nextlatest_cvopublished", "stor_nextlatest_cvopublished", "stor_nextlatest_cvopublished", "stor_dlychnge", "meancfs_dlychnge", "meancfs_dlyinflowobs_nextlatest_cvopublished",
+           "meancfs_fnf_nextlatest_cvopublished")
 
 unit <- c("taf", "taf", "taf", "taf", "taf", "taf",
           "taf", "taf", "taf", "taf", "cfs", "cfs",
           "cfs")
-friant_fnf_nextmostrecent <- cbind(friant_fnf_nextmostrecent_t, nws_id, param, unit)
+friant_fnf_nextmostrecent <- cbind(friant_fnf_nextmostrecent_t, nws_id, usbr_web_param, unit)
+rm(friant_fnf_nextmostrecent_t, unit, usbr_web_param, nws_id)
 as_tibble(friant_fnf_nextmostrecent)
 
 friant_fnf_nextmostrecent$value <- gsub(",", "", friant_fnf_nextmostrecent$value )
 friant_fnf_nextmostrecent$value <- gsub("+", "", friant_fnf_nextmostrecent$value ) #doesn't work, but below's as.numeric() seems to
 as_tibble(friant_fnf_nextmostrecent)
-friant_fnf_nextmostrecent <- friant_fnf_nextmostrecent %>% mutate(value = as.numeric(value))
+friant_fnf_nextmostrecent <- friant_fnf_nextmostrecent %>% mutate(value = as.numeric(value)) 
 as_tibble(friant_fnf_nextmostrecent)
-
-
+friant_fnf_nextmostrecent <- friant_fnf_nextmostrecent %>%
+                             filter(usbr_web_param != "stor_dlychnge") %>% filter(usbr_web_param != "meancfs_dlychnge")
+as_tibble(friant_fnf_nextmostrecent)
 }
 as_tibble(friant_fnf_mostrecent)
 as_tibble(friant_fnf_nextmostrecent)
 
-friant_fnf_nextmostrecent <- friant_fnf_nextmostrecent %>% mutate(prev_value = value)
-                             transmute(res, prev_value)
 
-as_tibble(friant_fnf_nextmostrecent)
+{
+friant_fnf_dlychnge <- inner_join(friant_fnf_mostrecent, friant_fnf_nextmostrecent, by = "res") 
+as_tibble(friant_fnf_dlychnge)
+friant_fnf_dlychnge <- friant_fnf_dlychnge %>% mutate(value = value.x - value.y)
+as_tibble(friant_fnf_dlychnge)
+friant_fnf_dlychnge <- friant_fnf_dlychnge %>% transmute(res, value, nws_id = nws_id.x, usbr_web_param = usbr_web_param.y, unit = unit.x)
+friant_fnf_dlychnge$usbr_web_param <- gsub("nextlatest", "dailychange", friant_fnf_dlychnge$usbr_web_param )
 
-df_diff <- left_join(friant_fnf_mostrecent, friant_fnf_nextmostrecent, by = "res")
-as_tibble(df_diff)
-df_diff <- df_diff %>% mutate(diff = value - prev_value)
-as_tibble(df_diff)
+as_tibble(friant_fnf_dlychnge)
+
+
+friant_fnf  <- rbind(friant_fnf_mostrecent, friant_fnf_nextmostrecent, friant_fnf_dlychnge)
+friant_fnf_dlychnge$res <- gsub("Millerton_stor_usbr", "Millerton_stor", friant_fnf_dlychnge$res )
+friant_fnf_dlychnge$res <- gsub("Millerton_observedinflow_cfs", "Millerton_inflow", friant_fnf_dlychnge$res )
+rm(friant_fnf_mostrecent, friant_fnf_nextmostrecent, friant_fnf_dlychnge)
+as_tibble(friant_fnf)
+
+
+
 ## capacities (from various sources online, including wiki) ##
 
 
-
+}
 
 res_id_nws <- c("TAEC1", "FLEC1", "HNTC1", "SAVC1", "MPLC1", "RGRC1", "BASC1", "KRHC1")
 res_cap <- c(125.0, 64.6, 88.834, 135.283, 123.0, 35.0, 45.4, 4.252)
 res_cap <- data.frame(res_id_nws, res_cap)
-
-
-
+rm(res_id_nws)
+as_tibble(res_cap)
+#}
